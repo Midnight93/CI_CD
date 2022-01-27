e1f9179774f894888e25cfaeb6a231e0df14f551
Midnight93 <edoardopiccari@gmail.com>
2022-01-27 11:34:40 -0800

    testing website

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
+# !/user/bin/env python
+# -*- coding:utf-8 -*-
+# time: 2018/3/7--19:49
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, world!"
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
